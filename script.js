26f828467a8ce0ae393c48e35206784ac768e9a5
Mubi9000 <shabbirmuhammad2000@gmail.com>
2023-05-16 18:11:18 -0400

    Added dynamic quotes and quotes authors

---
+++ script.js
@@ -0,0 +1,34 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
+
+let apiQuotes = [];
+
+//Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text
+}
+
+// Get Quotes From API
+async function getQuotes() {
+  const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
+  try {
+    const response = await fetch(apiURL)
+    apiQuotes = await response.json()
+    newQuote()
+  } catch (error) {
+    
+    // Catch Error Here
+  }
+}
+
+//On Load
+getQuotes();
+
+
