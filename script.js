fbec0dca14706475621efb5cf06b781b4757053c
Mubi9000 <shabbirmuhammad2000@gmail.com>
2023-05-17 21:21:22 -0400

    Added loading spinners and some color changes

--- script.js
+++ script.js
@@ -3,12 +3,24 @@ const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
-
+const loader = document.getElementById('loader')
 
 let apiQuotes = [];
 
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
+
 //Show New Quote
 function newQuote() {
+  showLoadingSpinner()
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
   //check if author field is blank and replace it wuth unknown
@@ -23,22 +35,27 @@ function newQuote() {
   } else {
     quoteText.classList.remove('long-quote')
   }
-
+  // Set Quote, Hide Loader
    quoteText.textContent = quote.text
+   removeLoadingSpinner()
 }
 
 // Get Quotes From API
+// Get Quotes From API
 async function getQuotes() {
+  showLoadingSpinner(); // Show the loader
   const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
   try {
-    const response = await fetch(apiURL)
-    apiQuotes = await response.json()
-    newQuote()
+    const response = await fetch(apiURL);
+    apiQuotes = await response.json();
+    newQuote();
+    removeLoadingSpinner();
   } catch (error) {
-    
+    console.log(error)
     // Catch Error Here
   }
 }
+
 
 // Tweet Quote
 function tweetQuote() {
